d8aeea7aa52d0bada975553d4363d599426dfea2
Phu <87368494+ppppHHHuuuu@users.noreply.github.com>
2023-09-10 07:42:30 +0700

    Demo Android Layout Types

---
+++ Lab_3/Project/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.week_3_assignment
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.linear_layout_activity)
+//        setContentView(R.layout.constraint_layout_activity)
+        setContentView(R.layout.relative_layout_activity)
+//        setContentView(R.layout.table_layout_activity)
+//        setContentView(R.layout.absolute_layout_activity)
+    }
+}
+
